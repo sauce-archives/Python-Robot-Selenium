afe4b44554ced4739579bc4dd71f4f10db8041ce
Lasse Häll <lasse@saucelabs.com>
2019-12-17 19:07:57 +0200

    Make sure Sauce Publisher sees console output

--- Resources/SauceLabs.py
+++ Resources/SauceLabs.py
@@ -7,12 +7,13 @@ access_key = os.environ.get('SAUCE_ACCESS_KEY')
 apibase = os.environ.get('DC_ENDPOINT')
 
 sauce_client = SauceClient(sauce_username=username, sauce_access_key=access_key, apibase=apibase)
+bi=BuiltIn()
 
 def report_sauce_status(name, status):
-    selenium = BuiltIn().get_library_instance('Selenium2Library')
+    selenium = bi.get_library_instance('Selenium2Library')
     job_id = selenium.driver.session_id
     passed = status == 'PASS'
     sauce_client.jobs.update_job(job_id, passed = passed, name = name)
-    print("SauceOnDemandSessionID={} job-name={}".format(job_id, name))
+    bi.log_to_console("\nSauceOnDemandSessionID={} job-name={}".format(job_id, name))
 
 
